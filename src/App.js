d7982a574b9ad4002fa55a61586f020ee2c34940
evvrit <turpin.elyse@gmail.com>
2023-02-03 11:47:23 -0500

    about section styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Pages
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
