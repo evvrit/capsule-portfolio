e9cf21244ec539865e6ef372b55f220f1e76daad
evvrit <turpin.elyse@gmail.com>
2023-03-03 15:46:29 -0500

    simplifying shared styles

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Section = styled.div`
-  min-height: 80vh;
+  min-height: 70vh;
   display: flex;
   align-items: center;
   justify-content: space-between;
