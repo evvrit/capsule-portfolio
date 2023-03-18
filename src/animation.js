0d4d5afaa8c41c03695d364a1baf24a83ec1ed6f
evvrit <turpin.elyse@gmail.com>
2023-03-18 14:52:02 -0400

    extended fade for smoother toggle transition

--- src/animation.js
+++ src/animation.js
@@ -1,6 +1,6 @@
 export const fade = {
   hidden: { opacity: 0 },
-  show: { opacity: 1, transition: { ease: "easeOut", duration: 0.3 } },
+  show: { opacity: 1, transition: { ease: "easeOut", duration: 0.5 } },
 };
 
 export const lineAnim = {
