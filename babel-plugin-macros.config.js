92f8fc659ae3d492df05c11f66f411b471d1013b
evvrit <turpin.elyse@gmail.com>
2023-03-21 13:41:57 -0400

    remove terser plugin

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,12 +1,5 @@
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
   "fontawesome-svg-core": {
     license: "free",
-  },
-  mode: "production",
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
   },
 };
