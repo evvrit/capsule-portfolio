9bc59b0e697ca5d6470fb90598ce93f5ee38a196
evvrit <turpin.elyse@gmail.com>
2023-03-21 13:38:28 -0400

    trying to reduce bundle size with terser webpack

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,5 +1,12 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
   "fontawesome-svg-core": {
     license: "free",
+  },
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
