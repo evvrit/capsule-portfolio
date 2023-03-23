307c35510a3e21d9f36b90968e658107589002a1
evvrit <turpin.elyse@gmail.com>
2023-03-23 17:51:16 -0400

    fixing call to hydrateRoot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { hydrateRoot } from "react-dom/client";
+import { hydrateRoot, hydrate } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-const root = hydrateRoot(document.getElementById("root"));
-root.hydrate(
+const allApp = (
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+hydrateRoot(document.getElementById("root"), allApp);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
