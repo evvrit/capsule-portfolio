ca421e51a4f020dd7ffe3deb5dfb5aace362c631
evvrit <turpin.elyse@gmail.com>
2023-03-29 11:38:48 -0400

    extremely wet routing but it works

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ import path from "path";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
+//pages
 import Home from "../src/pages/AboutUs";
+import Work from "../src/pages/MyWork";
+import Project from "../src/pages/Project";
+import Contact from "../src/pages/ContactUs";
 
 const app = express();
 const PORT = process.env.PORT || 3005;
@@ -19,6 +23,61 @@ app.use("^/$", (req, res) => {
     const html = renderToString(
       <StaticRouter location={req.url}>
         <Home />
+      </StaticRouter>
+    );
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
+    );
+  });
+});
+
+app.use("/work/", (req, res) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+
+    const html = renderToString(
+      <StaticRouter>
+        <Work />
+      </StaticRouter>
+    );
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
+    );
+  });
+});
+app.use("/contact/", (req, res) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+
+    const html = renderToString(
+      <StaticRouter>
+        <Contact />
+      </StaticRouter>
+    );
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
+    );
+  });
+});
+app.use("/work/:id", (req, res) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+
+    const html = renderToString(
+      <StaticRouter>
+        <Project />
       </StaticRouter>
     );
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/client";
 
 const allApp = (
   <React.StrictMode>
